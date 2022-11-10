1c74e3fa680b56bc55c4aaa5a9c74ac76975dc33
ngynbngoc <nnbn2411@gmail.com>
2022-11-10 10:07:07 +0700

    add remove spaces in the beginning & ending

---
+++ RemoveSpacesRule/RemoveSpacesIntheBegin_End.cs
@@ -0,0 +1,18 @@
+﻿using Contract;
+
+namespace RemoveSpacesRule
+{
+    public class RemoveSpacesIntheBegin_End : IRule
+    {
+        public string Name => "RemoveSpacesIntheBegin_End";
+
+        public string Config => "";
+
+        public string Parse { get; set; }
+
+        public string Rename(string inp)
+        {
+            return inp.Trim();
+        }
+    }
+}
