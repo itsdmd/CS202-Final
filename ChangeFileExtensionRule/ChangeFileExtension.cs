a44237561149c3af4b978efda950215febc9c2bf
ngynbngoc <nnbn2411@gmail.com>
2022-11-10 10:12:05 +0700

    Add change file extention rule

---
+++ ChangeFileExtensionRule/ChangeFileExtension.cs
@@ -0,0 +1,25 @@
+﻿using Contract;
+
+namespace ChangeFileExtensionRule
+{
+    public class ChangeFileExtension : IRule
+    {
+        public string Name => "ChangeFileExtension";
+
+        private string NewExtension = "doc";
+        public string Config => NewExtension;
+        public string Parse
+        {
+            set
+            {
+                var words = value.Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1);
+                NewExtension = string.Join(" ", words);
+            }
+        }
+
+        public string Rename(string origin)
+        {
+            return Path.ChangeExtension(origin, NewExtension);
+        }
+    }
+}
