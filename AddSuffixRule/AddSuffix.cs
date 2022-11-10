f2c0d79090ce985b1ab262e300da2098a7f88602
ngynbngoc <nnbn2411@gmail.com>
2022-11-10 10:14:54 +0700

    add suffix rule

---
+++ AddSuffixRule/AddSuffix.cs
@@ -0,0 +1,32 @@
+﻿using Contract;
+using System.Text;
+
+namespace AddSuffixRule
+{
+    public class AddSuffix : IRule
+    {
+        public string Name => "AddSuffix";
+        public string Config => Suffix;
+
+        private string Suffix = "suffix";
+        public string Parse
+        {
+            set
+            {
+                // Remove the first word from the string and take the rest as the suffix
+                var words = value.Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1);
+                Suffix = string.Join(" ", words);
+            }
+        }
+
+        public string Rename(string origin)
+        {
+            var sb = new StringBuilder();
+            int i = origin.IndexOf('.');
+            sb.Append(origin);
+            sb.Insert(i, Suffix);
+            return sb.ToString();
+
+        }
+    }
+}
