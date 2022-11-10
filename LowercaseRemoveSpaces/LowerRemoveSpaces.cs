d16eef90a8ebf035852674fcffc6f127fe6416ba
ngynbngoc <nnbn2411@gmail.com>
2022-11-10 10:10:17 +0700

    add lowercase rule

---
+++ LowercaseRemoveSpaces/LowerRemoveSpaces.cs
@@ -0,0 +1,20 @@
+﻿using Contract;
+
+namespace LowercaseAndRemoveAllSpacesRule
+{
+    public class LowerRemoveSpaces : IRule
+    {
+        public string Name => "LowerRemoveSpaces";
+
+        public string Parse { get; set; }
+
+        public string Config => "";
+
+        public string Rename(string origin)
+        {
+            origin = origin.ToLower();
+            origin = origin.Replace(" ", "");
+            return origin;
+        }
+    }
+}
